e5b4d6647ee129b88fccb98963ea02dbd9b1a72e
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-07-23 19:55:00 -0300

    deploy

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,17 @@
 import {  ethers} from 'ethers'
 
 export default async function ({ store, redirect,app,route }) {
-    app.$util.refreshAuthToken()
-    const provider = new ethers.providers.Web3Provider((window).ethereum )
-    const addresses = await provider.listAccounts();
-    if (addresses.length) {
-        store.state.wallet=addresses[0]
-    } else {
-        store.state.wallet=null
+    try{
+        app.$util.refreshAuthToken()
+        const provider = new ethers.providers.Web3Provider((window).ethereum )
+        const addresses = await provider.listAccounts();
+        if (addresses.length) {
+            store.state.wallet=addresses[0]
+        } else {
+            store.state.wallet=null
+        }
+    }catch(e){
+        console.log(e)
     }
+    
 }
